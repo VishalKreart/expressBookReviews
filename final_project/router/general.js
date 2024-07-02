87ea87210670944e39e0b39f6784c658b405e321
VishalKreart <vishalsanap28@gmail.com>
2024-07-02 08:03:43 -0400

    Tast 10 to 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,12 +35,43 @@ public_users.get('/',function (req, res) {
   return res.send(JSON.stringify({books},null,4));
 });
 
+//task 10 
+public_users.get('/books',(req,res)=>{
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
+})
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbnCode = req.params.isbn;
   res.send(books[isbnCode]);
  });
   
+// TASK 11 - Get book details based on ISBN using Promises
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const get_books_isbn = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    // console.log(isbn);
+        if (req.params.isbn <= 10) {
+        resolve(res.send(books[isbn]));
+    }
+        else {
+            reject(res.send('ISBN not found'));
+        }
+    });
+    get_books_isbn.
+        then(function(){
+            console.log("Promise for Task 11 is resolved");
+   }).
+        catch(function () { 
+                console.log('ISBN not found');
+  });
+
+});
+
 // Get book details based on author
 function getBooksByAuthor(authorName) {
     let matchingBooks = [];
@@ -64,6 +95,30 @@ public_users.get('/author/:author',function (req, res) {
   let booksByAuthorName = getBooksByAuthor(authorName);
   return res.send(booksByAuthorName);
 });
+// TASK 12 - Get book details based on author
+public_users.get('/books/author/:author',function (req, res) {
+
+    const get_books_author = new Promise((resolve, reject) => {
+
+        const authorName = req.params.author;
+        let booksByAuthorName = getBooksByAuthor(authorName);
+    if (booksByAuthorName.length >0) {
+        resolve(res.send(JSON.stringify({booksByAuthorName}, null, 4)));
+    } else {
+        reject(res.send("The mentioned author does not exist "))    
+    }
+    
+        
+    });
+
+    get_books_author.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned author does not exist');
+  });
+
+  });
+
 
 // Get all books based on title
 function getBooksByTitle(titleName) {
@@ -86,6 +141,30 @@ public_users.get('/title/:title',function (req, res) {
   let booksByTitleName = getBooksByTitle(titleName);
   return res.send(booksByTitleName);
 });
+
+// TASK 13 - Get book details based on Title
+public_users.get('/books/title/:title',function (req, res) {
+
+    const get_books_title = new Promise((resolve, reject) => {
+
+        const titleName = req.params.title;
+        let booksByTitle = getBooksByTitle(titleName);
+    if (booksByTitle.length >0) {
+        resolve(res.send(JSON.stringify({booksByTitle}, null, 4)));
+    } else {
+        reject(res.send("The mentioned title does not exist "))    
+    }
+    
+        
+    });
+
+    get_books_title.then(function(){
+            console.log("Promise is resolved");
+   }).catch(function () { 
+                console.log('The mentioned title does not exist');
+  });
+
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
